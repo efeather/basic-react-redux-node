0c5af1a5dbb7ac34df67364dbb3625f9b7ecd002
Eddie Feather <eddie.r.fether@gmail.com>
2020-04-04 16:20:30 -0500

    Added selenium and configered basic driver

---
+++ src/test/integration/gui/business/driver.ts
@@ -0,0 +1,26 @@
+import 'chromedriver';
+import { Builder, logging, WebDriver } from 'selenium-webdriver'
+
+let driver:WebDriver|null = null;
+
+export const getDriver = ():WebDriver =>{
+    if(!driver){
+        const loggingPrefs:logging.Preferences = new logging.Preferences();
+        loggingPrefs.setLevel(logging.Type.BROWSER,logging.Level.ALL);
+
+        driver = new Builder()
+            .forBrowser('chrome')
+            .withCapabilities(loggingPrefs)
+            .build();
+    }
+
+    return driver;
+};
+
+export const closeDriver = () =>{
+    getDriver()
+        .close()
+        .then(()=> driver = null)
+};
+
+
